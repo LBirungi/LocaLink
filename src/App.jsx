855292bb2089ad565f2ca0fff4660a4478ff7245
LBirungi <lillian_birungi_lb@yahoo.co.uk>
2023-11-28 01:35:05 +0300

    Add Layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { AppShell, Burger, Group, Skeleton, Text } from '@mantine/core';
+import Header from './assets/components/Header';
+import Navbar from './assets/components/Navbar';
+import Main from './assets/components/Main';
+import Aside from './assets/components/Aside';
+import Footer from './assets/components/Footer';
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
-    <>
     <MantineProvider>
     <AppShell
       layout="alt"
@@ -19,31 +23,13 @@ function App() {
       aside={{ width: 300, breakpoint: 'md', collapsed: { desktop: false, mobile: true } }}
       padding="md"
     >
-      <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-        </Group>
-      </AppShell.Header>
-      <AppShell.Navbar p="md">
-        <Group>
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <Text>Localink</Text>
-        </Group>
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
-      </AppShell.Navbar>
-      <AppShell.Main>
-        Alt layout – Navbar and Aside are rendered on top on Header and Footer
-      </AppShell.Main>
-      <AppShell.Aside p="md">Aside</AppShell.Aside>
-      <AppShell.Footer p="md">Footer</AppShell.Footer>
+      <Header opened={opened} toggle={toggle} />
+      <Navbar opened={opened} toggle={toggle} />
+      <Main/>
+      <Aside/>
+      <Footer/>
     </AppShell>
-     
-    </MantineProvider>;
-    </>
+    </MantineProvider>
   )
 }
 
