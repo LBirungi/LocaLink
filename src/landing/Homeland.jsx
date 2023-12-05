d46a5043da1315400b3cf0c3667d750d3206303f
LBirungi <lillian_birungi_lb@yahoo.co.uk>
2023-12-05 18:42:50 +0300

    separate concerns

--- src/landing/Homeland.jsx
+++ src/landing/Homeland.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { Container, BackgroundImage, MantineProvider } from "@mantine/core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faTwitter, faLinkedin, faGithub, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import backgroundImage from "/vite.svg"; // Corrected path to backgroundImage
-
 import Headerland from "./Headerland";
 import Footerland from "./Footerland";
 
@@ -38,50 +34,13 @@ function Homeland() {
                 </div>
               </div>
             </Container>
+           
           </main>
-          <footer>
-            <div
-              style={{
-                backgroundColor: "#333",
-                color: "#fff",
-                padding: "20px",
-                textAlign: "center",
-              }}
-            >
-              This is the footer. Customize this section according to your
-              requirements.
-              <section id="contact">
-                <div id="contact-title">
-                  <h1>Connect With Me</h1>
-                </div>
-                <div id="contacticons">
-                  <a href="#" className="fa fa-facebook">
-                    <FontAwesomeIcon icon={faFacebook} />
-                  </a>
-                  <a href="#" className="fa fa-twitter">
-                    <FontAwesomeIcon icon={faTwitter} />
-                  </a>
-                  <a href="#" className="fa fa-linkedin-square">
-                    <FontAwesomeIcon icon={faLinkedin} />
-                  </a>
-                  <a href="#" className="fa fa-github">
-                    <FontAwesomeIcon icon={faGithub} />
-                  </a>
-                  <a href="#" className="fa fa-whatsapp">
-                    <FontAwesomeIcon icon={faWhatsapp} />
-                  </a>
-                  <a href="#" className="glyphicon glyphicon-envelope">
-                    <FontAwesomeIcon icon={faEnvelope} />
-                  </a>
-                </div>
-                <div>
-                  <h4>Copyright ©2023, Localink</h4>
-                </div>
-              </section>
-            </div>
-          </footer>
+        
         </div>
       </BackgroundImage>
+
+      <Footerland/>
     </MantineProvider>
   );
 }
