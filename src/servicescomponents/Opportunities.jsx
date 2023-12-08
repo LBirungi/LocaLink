2db04b585016599aecd64652fa55173d4dd51dc7
Johnsonbyaruhanga <johnsonbyaruhanga2016@gmail.com>
2023-12-08 12:41:19 -0800

    opportunities

--- src/servicescomponents/Opportunities.jsx
+++ src/servicescomponents/Opportunities.jsx
@@ -1,13 +1,17 @@
+import React from "react";
+import Headeropp from "./Headeropp";
+import Navopp from "./Navopp";
+import Footeropp from "./Footeropp";
+
 function Opportunities() {
-    return (
-      <>
-        <Navopp />
-        <Headeropp />
-  
-        <Footeropp />
-      </>
-    );
-  }
-  
-  export default Opportunities;
-  
+  return (
+    <>
+      <Navopp />
+      <Headeropp />
+
+      <Footeropp />
+    </>
+  );
+}
+
+export default Opportunities;
