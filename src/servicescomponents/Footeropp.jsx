43e80e976f892cd72f3bfc48f2ec8d23f81568fb
Johnsonbyaruhanga <johnsonbyaruhanga2016@gmail.com>
2023-12-08 12:28:27 -0800

    Navopp

--- src/servicescomponents/Footeropp.jsx
+++ src/servicescomponents/Footeropp.jsx
@@ -54,7 +54,7 @@ function Footeropp() {
           </section>
         </div>
       </footer>
-     </>
+    </>
   );
 }
 export default Footeropp;
--- src/servicescomponents/Navopp.jsx
+++ src/servicescomponents/Navopp.jsx
@@ -1,8 +1,61 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+  faGithub,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Bold } from "tabler-icons-react";
 
-function Navopp() {
+function Footeropp() {
   return (
     <>
+      <footer>
+        <div
+          style={{
+            backgroundColor: "#000000",
+            color: "#fff",
+            padding: "20px",
+            textAlign: "center",
+            font: "Bold",
+          }}
+        >
+          Enjoy your Experience with LocaLink
+          <section id="contact">
+            <div id="contact-title">
+              <h1>Connect With Me</h1>
+            </div>
+            <div id="contacticons">
+              <a href="#" className="fa fa-facebook">
+                <FontAwesomeIcon icon={faFacebook} />
+              </a>
+              <a href="#" className="fa fa-twitter">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+              <a href="#" className="fa fa-linkedin-square">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+              <a href="#" className="fa fa-github">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="#" className="fa fa-whatsapp">
+                <FontAwesomeIcon icon={faWhatsapp} />
+              </a>
+              <a href="#" className="glyphicon glyphicon-envelope">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </div>
+            <div>
+              <h4>Copyright ©2023, Localink</h4>
+            </div>
+          </section>
+        </div>
+      </footer>
     </>
   );
 }
-export default Navopp;
+export default Footeropp;
+g
