1032e6a43299ff901c38b60e48f1d9b9cd9a3c21
LBirungi <lillian_birungi_lb@yahoo.co.uk>
2023-12-14 13:02:36 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,20 @@ import Homeland from "./landing/Homeland.jsx";
 import Opportunities from "./servicescomponents/Opportunities.jsx";
 import Signup from "./Signup.jsx";
 import AdultLearning from "./learningcomponents/adultlearning.jsx";
+import PostPage from "./components/PostPage.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homeland />} /> 
-         <Route path="home" element={<Home />} /> 
+         {/* <Route path="home" element={<Home />} />  */}
          <Route path="signup"  element={<Signup/>} />
-        <Route path="discover" element={<Discover/>} />
+        {/* <Route path="discover" element={<Discover/>} /> */}
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="opportunities" element={<Opportunities/>} />
-        <Route path="dashboard" element={<Dashboard />} />
         <Route path="adultLearning" element={<AdultLearning/>}/>
+        <Route path="posts/:slug" element={<PostPage />} />
         
       </Routes>
     </Router>
